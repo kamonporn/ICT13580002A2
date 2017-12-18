361ae105275909fa9ed471d08fb96638ef6666be
zyxrox0 <thongdeelert_k@silpakorn.edu>
2017-12-18 17:59:54 +0700

    two

---
+++ ICT13580002A2/ICT13580002A2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580002A2
+{
+    public partial class ICT13580002A2Page : ContentPage
+    {
+        public ICT13580002A2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
